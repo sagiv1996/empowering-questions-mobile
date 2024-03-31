4c82c1407367d1e576cfe3abe29e05b44546a12a
Sagiv <sagivb13@gmail.com>
2024-03-31 18:19:12 +0300

    fix the error with sharing question

--- lib/view/components/question_widget.dart
+++ lib/view/components/question_widget.dart
@@ -20,84 +20,84 @@ class QuestionWidget extends StatelessWidget {
       this.openInNewPageIsAvailable = false});
 
   _handlerShareScreen() async {
-    Uint8List? image = await _screenshotController.capture();
+    String questionString = question.string;
+    Uint8List? image = await _screenshotController.captureFromWidget(Center(
+      child: Text(questionString),
+    ));
+
     await Share.shareXFiles([
-      XFile.fromData(image!.buffer.asUint8List(),
-          name: "screenShot.jpg", mimeType: "image/jpeg"),
-    ], text: "I'd to share with you a empowering question!");
+      XFile.fromData(image.buffer.asUint8List(),
+          name: "${question.$_id}.jpg", mimeType: "image/jpeg"),
+    ], text: questionString);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Screenshot(
-      controller: _screenshotController,
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.75,
-        padding: const EdgeInsets.all(10),
-        child: Card(
-          elevation: 5,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                question.string,
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () => _handlerShareScreen(),
-                    icon: const Icon(Icons.share),
-                    color: Colors.purple,
-                  ),
-                  Mutation$updateUserIdsLikes$Widget(
-                      builder: (runMutation, result) => Badge.count(
-                            isLabelVisible: question.countUsersLikes > 0,
-                            count: question.countUsersLikes,
-                            child: IconButton(
-                                onPressed: () {
-                                  onFavoriteTap!(
-                                      !question.doesUserLikeQuestion,
-                                      question.doesUserLikeQuestion
-                                          ? question.countUsersLikes - 1
-                                          : question.countUsersLikes + 1);
-                                  runMutation(
-                                      Variables$Mutation$updateUserIdsLikes(
-                                          questionId: question.$_id,
-                                          action: question.doesUserLikeQuestion
-                                              ? Enum$UserAction.REMOVE
-                                              : Enum$UserAction.ADD));
-                                },
-                                icon: Icon(
-                                  question.doesUserLikeQuestion
-                                      ? Icons.favorite
-                                      : Icons.favorite_outline,
-                                  color: Colors.red,
-                                )),
-                          )),
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.report,
-                        color: Colors.redAccent,
-                      )),
-                  if (openInNewPageIsAvailable)
-                    IconButton(
-                        onPressed: () {
-                          context.push('/question/${question.$_id}');
-                        },
-                        icon: const Icon(
-                          Icons.link,
-                          color: Colors.lightGreen,
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.75,
+      padding: const EdgeInsets.all(10),
+      child: Card(
+        elevation: 5,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              question.string,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () => _handlerShareScreen(),
+                  icon: const Icon(Icons.share),
+                  color: Colors.purple,
+                ),
+                Mutation$updateUserIdsLikes$Widget(
+                    builder: (runMutation, result) => Badge.count(
+                          isLabelVisible: question.countUsersLikes > 0,
+                          count: question.countUsersLikes,
+                          child: IconButton(
+                              onPressed: () {
+                                onFavoriteTap!(
+                                    !question.doesUserLikeQuestion,
+                                    question.doesUserLikeQuestion
+                                        ? question.countUsersLikes - 1
+                                        : question.countUsersLikes + 1);
+                                runMutation(
+                                    Variables$Mutation$updateUserIdsLikes(
+                                        questionId: question.$_id,
+                                        action: question.doesUserLikeQuestion
+                                            ? Enum$UserAction.REMOVE
+                                            : Enum$UserAction.ADD));
+                              },
+                              icon: Icon(
+                                question.doesUserLikeQuestion
+                                    ? Icons.favorite
+                                    : Icons.favorite_outline,
+                                color: Colors.red,
+                              )),
                         )),
-                ],
-              ),
-              footer ?? const SizedBox()
-            ],
-          ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.report,
+                      color: Colors.redAccent,
+                    )),
+                if (openInNewPageIsAvailable)
+                  IconButton(
+                      onPressed: () {
+                        context.push('/question/${question.$_id}');
+                      },
+                      icon: const Icon(
+                        Icons.link,
+                        color: Colors.lightGreen,
+                      )),
+              ],
+            ),
+            footer ?? const SizedBox()
+          ],
         ),
       ),
     );
