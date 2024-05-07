143f7375cad9377a75defbb28d3e9bf7b6f36e00
Sagiv <sagivb13@gmail.com>
2024-05-07 17:48:09 +0300

    write new registerPage

--- lib/view/pages/register_page.dart
+++ lib/view/pages/register_page.dart
@@ -1,13 +1,9 @@
+import 'package:choice/choice.dart';
 import 'package:empowering_questions_mobile/api/user.dart';
 import 'package:empowering_questions_mobile/heberw_string.dart';
-import 'package:empowering_questions_mobile/provider/chat_register_provider.dart';
-import 'package:empowering_questions_mobile/provider/interfaces/chat_provider.interface.dart';
-import 'package:empowering_questions_mobile/provider/interfaces/chat_register_provider_interface.dart';
 import 'package:empowering_questions_mobile/provider/user_provider.dart';
-import 'package:empowering_questions_mobile/view/components/custom_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:group_button/group_button.dart';
 import 'package:provider/provider.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -18,161 +14,104 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _TestState extends State<RegisterPage> {
-  final GroupButtonController _checkboxesController = GroupButtonController();
-  final ScrollController _scrollController = ScrollController();
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _addMessages([
-        HebrewString.registerPageMessageNumber1,
-        HebrewString.registerPageMessageNumber2,
-        HebrewString.registerPageMessageNumber3
-      ], true);
-      context.read<ChatRegisterProvider>().changeTurn();
-      context.read<ChatRegisterProvider>().nextStep();
-    });
-  }
+  List<CategoryOptions> selectedCategories = [];
+  GenderOptions? selectedGender;
+  FrequencyOptions? selectedFrequency;
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-      _scrollController.animateTo(
-        _scrollController.position.maxScrollExtent,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
-    });
-    ChatRegisterProvider chat = context.watch<ChatRegisterProvider>();
-    List<Message> messages = chat.messages;
-    bool? isRadio = chat.suggestions?.values.last;
     return Scaffold(
         appBar: AppBar(),
         body: Column(
           children: [
-            Expanded(
-                child: ListView.builder(
-                    controller: _scrollController,
-                    itemCount: messages.length,
-                    itemBuilder: (context, index) =>
-                        CustomBubble(message: messages[index]))),
-            if (chat.turn == Turn.human)
-              if (chat.step != SuggestionsTypes.finish)
-                BottomSheet(
-                  onClosing: () {},
-                  builder: (context) {
-                    return Column(
-                      children: [
-                        Card(
-                            child: GroupButton(
-                          controller: _checkboxesController,
-                          isRadio: isRadio!,
-                          options: GroupButtonOptions(
-                            unselectedTextStyle: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                          onSelected: (value, index, isSelected) {
-                            if (isRadio) {
-                              chat.changeTurn();
-                              _addMessage(
-                                  HebrewString.registerPageGetAtHebrew(value),
-                                  false);
-
-                              print(value.toString());
-                              switch (chat.step) {
-                                case SuggestionsTypes.selectGender:
-                                  chat.setSelectedGender(value);
-                                  _addMessages([
-                                    HebrewString.registerPageMessageNumber4,
-                                    HebrewString.registerPageMessageNumber5
-                                  ], true);
-                                  break;
-                                case SuggestionsTypes.selectFrequency:
-                                  chat.setSelectedFrequency(value);
-                                  _addMessages([
-                                    HebrewString.registerPageMessageNumber9,
-                                    HebrewString.registerPageMessageNumber10,
-                                  ], true);
-
-                                  break;
-                                default:
-                              }
-                              chat.changeTurn();
-                              chat.nextStep();
-                              _resetSelectedIndex();
-                            }
-                          },
-                          buttons: chat.suggestions!.keys.first,
-                          buttonTextBuilder: (selected, value, context) =>
-                              HebrewString.registerPageGetAtHebrew(value),
-                        )),
-                        if (!isRadio)
-                          TextButton(
-                              onPressed: () {
-                                chat.setSelectedCategories(CategoryOptions
-                                    .values
-                                    .where((element) => _checkboxesController
-                                        .selectedIndexes
-                                        .contains(CategoryOptions.values
-                                            .indexOf(element)))
-                                    .toList());
-                                List<String> categoriesShouldAddToScreen = chat
-                                    .selectedCategories
-                                    .map((category) =>
-                                        HebrewString.registerPageGetAtHebrew(
-                                            category))
-                                    .toList()
-                                    .cast();
-                                _addMessages(
-                                    categoriesShouldAddToScreen, false);
-                                _addMessages([
-                                  HebrewString.registerPageMessageNumber6,
-                                  HebrewString.registerPageMessageNumber7,
-                                  HebrewString.registerPageMessageNumber8
-                                ], true);
-                                chat.nextStep();
-                                _resetSelectedIndex();
-                              },
-                              child: const Text(
-                                  HebrewString.registerPageMessageNumber11))
-                      ],
-                    );
-                  },
-                )
-              else
-                ElevatedButton(
-                    onPressed: () {
-                      try {
-                        context.read<UserProvider>().createUser({
-                          "frequency": chat.selectedFrequency.name,
-                          "gender": chat.selectedGender.name,
-                          "categories": chat.selectedCategories
-                              .map((e) => e.name)
-                              .toList()
-                        }).then((value) => context.push("/"));
-                      } catch (e) {
-                        print("ERROR!! $e");
-                      }
-                    },
-                    child: const Text(HebrewString.registerPageRegisterButton))
+            InlineChoice<GenderOptions>.single(
+              clearable: true,
+              value: selectedGender,
+              itemCount: GenderOptions.values.length,
+              onChanged: (value) {
+                setState(() {
+                  selectedGender = value;
+                });
+              },
+              itemBuilder: (state, i) {
+                return ChoiceChip(
+                  selected: state.selected(GenderOptions.values[i]),
+                  onSelected: state.onSelected(GenderOptions.values[i]),
+                  label: Text(HebrewString.registerPageGetAtHebrew(
+                      GenderOptions.values[i])),
+                );
+              },
+              listBuilder: ChoiceList.createScrollable(
+                spacing: 10,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 7),
+              ),
+            ),
+            InlineChoice<CategoryOptions>.multiple(
+                clearable: true,
+                value: selectedCategories,
+                itemCount: CategoryOptions.values.length,
+                onChanged: (value) {
+                  setState(() {
+                    selectedCategories = value;
+                  });
+                },
+                itemBuilder: (state, i) {
+                  return ChoiceChip(
+                    selected: state.selected(CategoryOptions.values[i]),
+                    onSelected: state.onSelected(CategoryOptions.values[i]),
+                    label: Text(HebrewString.registerPageGetAtHebrew(
+                        CategoryOptions.values[i])),
+                  );
+                },
+                listBuilder: ChoiceList.createWrapped(
+                  alignment: WrapAlignment.center,
+                  spacing: 10,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 7),
+                )),
+            InlineChoice<FrequencyOptions>.single(
+              clearable: true,
+              value: selectedFrequency,
+              itemCount: FrequencyOptions.values.length,
+              onChanged: (value) {
+                setState(() {
+                  selectedFrequency = value;
+                });
+              },
+              itemBuilder: (state, i) {
+                return ChoiceChip(
+                  selected: state.selected(FrequencyOptions.values[i]),
+                  onSelected: state.onSelected(FrequencyOptions.values[i]),
+                  label: Text(HebrewString.registerPageGetAtHebrew(
+                      FrequencyOptions.values[i])),
+                );
+              },
+              listBuilder: ChoiceList.createScrollable(
+                spacing: 10,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 7),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  try {
+                    print("selectedFrequency $selectedFrequency");
+                    print("selectedGender $selectedGender");
+                    print("selectedCategories $selectedCategories");
+                    await context.read<UserProvider>().createUser(
+                        frequency: selectedFrequency!,
+                        categories: selectedCategories,
+                        gender: selectedGender!);
+                    if (context.mounted) {
+                      context.push("/");
+                    }
+                  } catch (e) {
+                    print("ERROR!! $e");
+                  }
+                },
+                child: const Text(HebrewString.registerPageRegisterButton))
           ],
         ));
-  }
-
-  void _addMessage(String message, bool isSender) {
-    ChatRegisterProvider chatRegisterProvider =
-        context.read<ChatRegisterProvider>();
-    chatRegisterProvider.addMessage(message, isSender);
-  }
-
-  void _addMessages(List<String> messages, bool isSender) {
-    ChatRegisterProvider chatRegisterProvider =
-        context.read<ChatRegisterProvider>();
-    chatRegisterProvider.addMessages(messages, isSender);
-  }
-
-  void _resetSelectedIndex() {
-    _checkboxesController.unselectAll();
   }
 }
