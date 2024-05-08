f5014f79a3c6b4bf03c78f98616381b5a6f5849a
Sagiv <sagivb13@gmail.com>
2024-05-08 10:23:51 +0300

    create and add QuestionsProviderInterface

--- lib/provider/questions_provider.dart
+++ lib/provider/questions_provider.dart
@@ -1,12 +1,18 @@
 import 'package:empowering_questions_mobile/api/question.dart';
 import 'package:empowering_questions_mobile/provider/http_request_provider.dart';
+import 'package:empowering_questions_mobile/provider/interfaces/questions_provider_interface.dart';
 
-class QuestionsProvider extends HttpRequestProvider {
-  final List<Question> _questions = [];
+class QuestionsProvider extends HttpRequestProvider
+    implements QuestionsProviderInterface {
+  List<Question> _questions = [];
+
+  @override
   List<Question> get questions => _questions;
+  @override
   List<String>? get excludeIds =>
       _questions.map((question) => question.id).toList();
 
+  @override
   Future<bool> fetchRandom() async {
     dio.options.headers = await getToken();
     final client = RestQuestion(dio);
@@ -14,12 +20,18 @@ class QuestionsProvider extends HttpRequestProvider {
     if (questions == null) {
       return true;
     }
-    _questions.addAll(questions);
+    setQuestions(questions);
     notifyListeners();
     return false;
   }
 
+  @override
   clearQuestion() {
     _questions.clear();
+  }
+
+  @override
+  void setQuestions(List<Question> questions) {
+    _questions = questions;
   }
 }
